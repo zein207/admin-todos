50469e9c9aecfa25361a381f011d686d87369a59
david <zein207@gmail.com>
2024-06-25 22:58:59 -0600

    Create params

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -3,7 +3,28 @@ import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: Request) { 
 
-    const todos = await prisma.todo.findMany();
+    const { searchParams } = new URL(request.url);
+    const take = +(searchParams.get('take') ?? '10');
+    const skip = +(searchParams.get('skip') ?? '0');
+
+    if( isNaN(take) ) {
+        return NextResponse.json(
+            { message: 'Take must be a number' },
+            { status: 400}
+        )
+    }
+
+    if( isNaN(skip) ) {
+        return NextResponse.json(
+            { message: 'skip must be a number' },
+            { status: 400}
+        )
+    }
+
+    const todos = await prisma.todo.findMany({
+        take: take,
+        skip: skip
+    });
 
     return NextResponse.json(todos);
 }
