9b44dbcffa4ec3a7db38be6a22f73f6ca9eefa47
david <zein207@gmail.com>
2024-06-26 21:57:32 -0600

    Add yup for POST validations

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(request: Request) { 
 
@@ -29,13 +30,27 @@ export async function GET(request: Request) {
     return NextResponse.json(todos);
 }
 
+const postSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false)
+})
+
 export async function POST(request: Request) {
 
-    const body = await request.json();
+    try {
+        const { complete, description } = await postSchema.validate( await request.json() );
+    
+        const todo = await prisma.todo.create({
+            data: {
+                complete,
+                description
+            }
+        })
+    
+        return NextResponse.json(todo);
+        
+    } catch (error) {
+        return NextResponse.json(error, { status: 400});
+    }
 
-    const todo = await prisma.todo.create({
-        data: body
-    })
-
-    return NextResponse.json(todo);
 }
